dfa9e4ed6300d7d1364b93a54a7eed6b44697cbe
wolframdeus <wolfram.deus@gmail.com>
2020-04-10 03:08:31 +0500

    Удалены неиспользованные ошибки

--- src/gql/errors.ts
+++ src/gql/errors.ts
@@ -3,11 +3,4 @@ import {ApolloError} from './types';
 
 export const AuthorizationError = createError(ApolloError.Authorization);
 
-export const AdminRightsRequiredError =
-  createError(ApolloError.AdminRightsRequired);
-
 export const UnknownError = createError(ApolloError.Unknown);
-
-export const UserNotFoundError = createError(ApolloError.UserNotFound);
-
-export const FindUsersLimitError = createError(ApolloError.FindUsersLimit);
